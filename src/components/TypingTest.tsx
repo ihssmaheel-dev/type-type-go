2c8e2766201d6ff38e803425193d5d16a8457585
ismail-hepl <ismail.s@hepl.com>
2024-05-14 21:50:14 +0530

    Refactor: Indentation fixes

--- src/components/TypingTest.tsx
+++ src/components/TypingTest.tsx
@@ -52,11 +52,12 @@ const TypingTest = () => {
 				{
 					paragraph.split("").map((char, index) => (
 						<span 
-							className={
-								`text-2xl leading-9 select-none cursor-text text-slate-600 
+							className={`
+								text-2xl leading-9 select-none cursor-text text-slate-600 
 								${index === charIndex ? "border-b-4 border-solid border-slate-900" : ""} 
 								${correctWrong[index] === "correct" ? "bg-green-500 text-slate-900" : ""} 
-								${correctWrong[index] === "wrong" ? "bg-red-500 text-slate-900" : ""}`} 
+								${correctWrong[index] === "wrong" ? "bg-red-500 text-slate-900" : ""}
+							`} 
 							ref={(e) => charRefs.current[index] = e}
 						>
 							{char}
