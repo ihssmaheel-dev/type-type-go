1ce8de396901d7248c0822e1803f52e07044d8ca
ismail-hepl <ismail.s@hepl.com>
2024-05-17 10:10:09 +0530

    Fix: Fix the wpm and cpm goes Infinity issue

--- src/Utils/helper.ts
+++ src/Utils/helper.ts
@@ -1,9 +1,11 @@
 export const calculateWPM = (correctChars: number, totalTime: number): number => { 
     const wpm = Math.round((correctChars / 5 / totalTime) * 60);
-    return wpm < 0 || isNaN(wpm) ? 0 : wpm;
+
+    return wpm < 0 || wpm === Infinity ? 0 : wpm;
 }
 
 export const calculateCPM = (correctChars: number, totalTime: number): number => {
     const cpm = Math.round((correctChars * (60 / totalTime)));
-    return cpm < 0 || isNaN(cpm) ? 0 : cpm;
+
+    return cpm < 0 || cpm === Infinity ? 0 : cpm;
 }
