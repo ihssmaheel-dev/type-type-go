a13f6a345079429bbb216fff0e0194370dc8f819
ismail-hepl <ismail.s@hepl.com>
2024-05-19 20:42:45 +0530

    Refactor: Removed the reset button and make sure there only stats

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -10,7 +10,6 @@ interface StatsProps {
 	accuracy: number;
 	WPM: number;
 	CPM: number;
-	reset: () => void;
 }
 
 const Stats: React.FC<StatsProps> = ({
@@ -20,20 +19,16 @@ const Stats: React.FC<StatsProps> = ({
 	accuracy,
 	WPM,
 	CPM,
-	reset
 }) => {
 	return (
-		<div className='w-10/12 flex text-lg font-bold text-slate-900 shadow select-none'>
-			<div className='w-full bg-gray-200 rounded-lg px-8 py-2 flex items-center justify-between mr-4'>
-				{
-					mode === "time" ? <p>Time Left: <strong>{timeLeft}</strong></p> : ""  
-				}
-				<p>Mistakes: <strong>{mistakes}</strong></p>
-				<p>Accuracy: <strong>{accuracy}</strong></p>
-				<p>WPM: <strong>{WPM}</strong></p>
-				<p>CPM: <strong>{CPM}</strong></p>
-			</div>
-			<ResetButton reset={reset}/>
+		<div className='w-full bg-gray-200 rounded-lg px-8 py-2 flex items-center justify-between mr-4'>
+			{
+				mode === "time" ? <p>Time Left: <strong>{timeLeft}</strong></p> : ""  
+			}
+			<p>Mistakes: <strong>{mistakes}</strong></p>
+			<p>Accuracy: <strong>{accuracy}</strong></p>
+			<p>WPM: <strong>{WPM}</strong></p>
+			<p>CPM: <strong>{CPM}</strong></p>
 		</div>
 	)
 }
