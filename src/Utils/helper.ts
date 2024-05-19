a253445f3e05b40b162bef4e92d7a802fee92972
ismail-hepl <ismail.s@hepl.com>
2024-05-19 16:08:29 +0530

    Feat: Introduce the calculateAccuracy function

--- src/Utils/helper.ts
+++ src/Utils/helper.ts
@@ -9,3 +9,10 @@ export const calculateCPM = (correctChars: number, totalTime: number): number =>
 
     return cpm < 0 || cpm === Infinity ? 0 : cpm;
 }
+
+export const calculateAccuracy = (charIndex : number, errors : number, typedChar: string, currentChar: string | null | undefined ): number => {
+    const correctChars = charIndex - errors + (typedChar === currentChar ? 1 : 0);
+    const totalTypedChars = charIndex + 1;
+
+    return parseFloat(((correctChars / totalTypedChars) * 100).toFixed(2));
+}
