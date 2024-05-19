f6bb9fdda07b7fd61e9ec8fa46e502108e95a035
ismail-hepl <ismail.s@hepl.com>
2024-05-19 23:40:42 +0530

    Feat: Render the new component in the TypingTestHeaderBar

--- src/components/TypingTestHeaderBar.tsx
+++ src/components/TypingTestHeaderBar.tsx
@@ -3,6 +3,8 @@ import TypingOptionsBar from './TypingOptionsBar';
 import ModeSelector from './ModeSelector';
 import ModeSelectorOptions from './ModeSelectorOptions';
 import ModeSelectorContainer from './ModeSelectorContainer';
+import RefreshButton from './RefreshButton';
+import SoundToggleButton from './SoundToggleButton';
 
 type ModeType = "time" | "words" | "lorem";
 
@@ -18,6 +20,7 @@ interface TypingTestHeaderBarProps {
 const TypingTestHeaderBar: React.FC<TypingTestHeaderBarProps> = ({ mode, maxWords, maxTime, onModeChange, onChangeTime, onChangeWords }) => {
 	return (
 		<div className='w-10/12 flex text-lg font-bold text-slate-500 shadow select-none'>
+			<SoundToggleButton />
 			<TypingOptionsBar />
 			<ModeSelectorContainer mode={mode} onModeChange={onModeChange} maxTime={maxTime} onChangeTime={onChangeTime} maxWords={maxWords} onChangeWords={onChangeWords}/>
 		</div>
