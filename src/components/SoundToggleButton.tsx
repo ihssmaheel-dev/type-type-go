02d869040c5f966cbf53819088e1005b0a6b7402
ismail-hepl <ismail.s@hepl.com>
2024-05-19 23:40:16 +0530

    Feat: Create a new component called SoundToggleButton to manage sound in the application

---
+++ src/components/SoundToggleButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaVolumeHigh } from 'react-icons/fa6'
+
+interface SoundToggleButtonProps {
+	
+}
+const SoundToggleButton: React.FC<SoundToggleButtonProps> = () => {
+	return (
+		<button type='button' className='px-3 py-2 outline-none border border-solid border-slate-200 bg-gray-200 rounded-md cursor-pointer text-slate-900 text-base transition-all duration-200 hover:bg-slate-900 hover:text-gray-200 hover:border-gray-200 mr-2'>
+			<FaVolumeHigh/>
+		</button>
+	)
+}
+
+export default SoundToggleButton
