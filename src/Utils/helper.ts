0065b094bf48aa74bd1983a8fce6089180b0d539
ismail-hepl <ismail.s@hepl.com>
2024-05-19 12:16:52 +0530

    Refactor: Add the 10_000 as a default totalTime for words or lorem method

--- src/Utils/helper.ts
+++ src/Utils/helper.ts
@@ -1,10 +1,10 @@
-export const calculateWPM = (correctChars: number, totalTime: number): number => { 
+export const calculateWPM = (correctChars: number, totalTime: number = 10000): number => { 
     const wpm = Math.round((correctChars / 5 / totalTime) * 60);
 
     return wpm < 0 || wpm === Infinity ? 0 : wpm;
 }
 
-export const calculateCPM = (correctChars: number, totalTime: number): number => {
+export const calculateCPM = (correctChars: number, totalTime: number = 10000): number => {
     const cpm = Math.round((correctChars * (60 / totalTime)));
 
     return cpm < 0 || cpm === Infinity ? 0 : cpm;
