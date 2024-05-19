07106d0e9100023cdd1caabcccd8dab5ee5e88cb
ismail-hepl <ismail.s@hepl.com>
2024-05-19 19:22:21 +0530

    Feat: Introduce new hook called "useTypingLogic" to manage typing-related state and logic

---
+++ src/hooks/useTypingLogic.ts
@@ -0,0 +1,101 @@
+import { useEffect, useRef, useState } from "react"
+import { calculateAccuracy, calculateCPM, calculateWPM } from "../utils/helper";
+
+type CorrectWrongType = "correct" | "wrong" | "";
+const useTypingLogic = (paragraph: string, maxTime: number, timeLeft: number) => {
+	const [charIndex, setCharIndex] = useState(0);
+	const [mistakes, setMistakes] = useState(0);
+	const [WPM, setWPM] = useState(0);
+	const [CPM, setCPM] = useState(0);
+	const [accuracy, setAccuracy] = useState(100);
+	const [correctWrong, setCorrectWrong] = useState<CorrectWrongType[]>([]);
+	const [isTyping, setIsTyping] = useState(false);
+	const [errors, setErrors] = useState(0);
+	const charRefs = useRef<(HTMLSpanElement | null)[]>([]);
+	
+
+	useEffect(() => {
+		setCorrectWrong(Array(paragraph.length).fill(""));
+		charRefs.current = Array(paragraph.length).fill(null);
+	}, [paragraph]);
+
+	useEffect(() => {
+		let interval: ReturnType<typeof setInterval> | undefined;
+		if(isTyping && timeLeft > 0) {
+			interval = setInterval(() => {
+				const correctChars = charIndex - mistakes;
+				const totalTime = maxTime - timeLeft;
+
+				setWPM(calculateWPM(correctChars, totalTime));
+				setCPM(calculateCPM(correctChars, timeLeft));
+			}, 1000);
+		} else {
+			setIsTyping(false);
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval);
+	}, [isTyping, timeLeft]);
+
+	const handleKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		const characters = charRefs.current;
+
+		if(isTyping && e.code === "Backspace") {
+			if(charIndex < 0) return;
+
+			setCharIndex((prevCharIndex) => prevCharIndex - 1);
+			const isWrong = correctWrong[charIndex - 1] === "wrong";
+			isWrong && setMistakes((prevMistakes) => prevMistakes - 1);
+
+			setCorrectWrong((prevCorrectWrong) => {
+				const newCorrectWrong = [...prevCorrectWrong];
+				newCorrectWrong[charIndex - 1] = "";
+
+				return newCorrectWrong;
+			});
+		}
+
+		if(e.key.length === 1) {
+			const typedChar = e.key;
+			const currentChar = characters[charIndex]?.textContent;
+
+			if(charIndex < characters.length && timeLeft > 0) {
+				if(!isTyping) setIsTyping(true);
+
+				setCharIndex((prevCharIndex) => prevCharIndex + 1);
+
+				setCorrectWrong((prevCorrectWrong) => {
+					const newCorrectWrong = [...prevCorrectWrong];
+					newCorrectWrong[charIndex] = typedChar === currentChar ? "correct" : "wrong";
+					if(typedChar !== currentChar) {
+						setMistakes((prevMistakes) => prevMistakes + 1);
+						setErrors((prevErrors) => prevErrors + 1);
+					}
+
+					return newCorrectWrong;
+				});
+
+				if(charIndex === characters.length - 1) setIsTyping(false);
+
+				setAccuracy(calculateAccuracy(charIndex, errors, typedChar, currentChar))
+			} else {
+				setIsTyping(false);
+			}
+		}
+	};
+
+	const resetTyping = () => {
+		setCharIndex(0);
+		setMistakes(0);
+		setWPM(0);
+		setCPM(0);
+		setAccuracy(100);
+		setCorrectWrong(Array(paragraph.length).fill(""));
+		charRefs.current = Array(paragraph.length).fill(null);
+		setErrors(0);
+	}
+
+	return { charIndex, charRefs, mistakes, WPM, CPM, accuracy, correctWrong, handleKeydown, resetTyping }
+}
+
+export default useTypingLogic;
