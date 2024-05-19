4fcdbf8bffc03d494ba9a290821ea0590bd445a6
ismail-hepl <ismail.s@hepl.com>
2024-05-19 23:26:33 +0530

    Feat: Introduce a new component called "ModeSelectorContainer" that has mode settings

---
+++ src/components/ModeSelectorContainer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ModeSelector from './ModeSelector';
+import ModeSelectorOptions from './ModeSelectorOptions';
+
+type ModeType = "time" | "words" | "lorem";
+
+interface ModeSelectorContainerProps {
+	mode: ModeType;
+	maxWords: number;
+	maxTime: number;
+	onModeChange: (mode: ModeType) => void;
+	onChangeTime: (time: number) => void;
+	onChangeWords: (words: number) => void;
+}
+
+const Divider: React.FC = () => {
+	return (
+		<span className="text-slate-600 text-xl mx-10 font-extrabold">|</span>
+	)
+}
+
+const ModeSelectorContainer: React.FC<ModeSelectorContainerProps> = ({ mode, maxWords, maxTime, onModeChange, onChangeTime, onChangeWords }) => {
+	const timeOptions = [15, 30, 60];
+	const wordOptions = [10, 25, 50, 100];
+
+	return (
+		<div className="w-full p-2 px-8 rounded-lg bg-gray-200 shadow text-lg font-bold flex items-center text-slate-500 select-none">
+			<ModeSelector mode={mode} onModeChange={onModeChange} />
+			<Divider />
+			<ModeSelectorOptions mode={mode} timeOptions={timeOptions} maxTime={maxTime} onChangeTime={onChangeTime} wordOptions={wordOptions} maxWords={maxWords} onChangeWords={onChangeWords}/>
+		</div>
+	)
+}
+
+export default ModeSelectorContainer
