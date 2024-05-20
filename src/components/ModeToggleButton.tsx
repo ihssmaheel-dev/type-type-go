76fbc64c8f98d919c23f72b7caee05cbd110c36f
ismail-hepl <ismail.s@hepl.com>
2024-05-20 10:59:54 +0530

    Feat: Add transition effect to the ModeToggleButton

--- src/components/ModeToggleButton.tsx
+++ src/components/ModeToggleButton.tsx
@@ -12,7 +12,7 @@ interface ModeToggleButtonProps {
 const ModeToggleButton: React.FC<ModeToggleButtonProps> = ({ mode, icon, isActive, onClick }) => {
 	return (
 		<button
-    className={`flex items-center justify-between ${isActive ? 'text-slate-900' : ''}`}
+    className={`flex items-center justify-between ${isActive ? 'text-slate-900' : ''} transition-all duration-200`}
     onClick={onClick}
   >
     {icon} {mode}
